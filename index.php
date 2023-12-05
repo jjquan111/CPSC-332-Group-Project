0370f2d17ebe21a3f8f489c7b8d273a1828804d5
gabrielcodina <gabrielcodina@gmail.com>
2023-12-04 22:06:45 -0800

    more connection code

--- index.php
+++ index.php
@@ -1,3 +1,63 @@
 <html>
-    <h1>hello world!</h1>
+<h1>hello world!</h1>
+<?php
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpass = '';
+$dbname = '332DB'; // Your database name
+
+function connectToDatabase($host, $user, $pass, $dbname = '')
+{
+    $mysqli = new mysqli($host, $user, $pass, $dbname);
+
+    if ($mysqli->connect_error) {
+        die('Connection failed: ' . $mysqli->connect_error);
+    }
+
+    return $mysqli;
+}
+
+function createDatabase($mysqli, $dbname)
+{
+    $createDbQuery = "CREATE DATABASE IF NOT EXISTS $dbname";
+
+    if ($mysqli->query($createDbQuery)) {
+        echo "Database $dbname created successfully";
+    } else {
+        die("Error creating database: " . $mysqli->error);
+    }
+}
+
+function createTable($mysqli)
+{
+    $createTableQuery = "
+            CREATE TABLE IF NOT EXISTS users (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                username VARCHAR(255) NOT NULL,
+                email VARCHAR(255) NOT NULL
+            )";
+
+    if ($mysqli->query($createTableQuery)) {
+        echo "Table 'users' created successfully";
+    } else {
+        die("Error creating table: " . $mysqli->error);
+    }
+}
+
+// Connect to MySQL server and create or select the database
+$mysqli = connectToDatabase($dbhost, $dbuser, $dbpass);
+
+// Check if the database exists and create if needed
+createDatabase($mysqli, $dbname);
+
+// Create or select the database
+$mysqli = connectToDatabase($dbhost, $dbuser, $dbpass, $dbname);
+
+// Create a sample table
+createTable($mysqli);
+
+// Close the connection to the database
+$mysqli->close();
+?>
+
 </html>
