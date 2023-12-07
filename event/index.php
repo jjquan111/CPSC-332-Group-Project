978dce951c48eeda3c397a8704dec1379517bbb1
Caleb <wolfertamer@gmail.com>
2023-12-07 13:45:04 -0800

    added organizer.php which allows organizers to edit their events

--- event/index.php
+++ event/index.php
@@ -1,3 +1,19 @@
 <?php
+require_once('../base.php');
+$conn = new mysqli($GLOBALS['servername'], $GLOBALS['username'], $GLOBALS['password'], $GLOBALS['database'], $GLOBALS['port']);
+require_once('organizer.php');
+require_once('getevent.php');
+$inject = [
+    'title'=>'whoa',
+    'body'=>''
+];
 
+if(isset($_GET['eventid'])) {
+    [$error,$details] = getEventDetails($_GET['eventid']);
+    if(isset($details) && $details['organizerID'] == issetor($_SESSION['userid'])) {
+        $inject = getOrganizer($details);
+    }
+}
+
+printMain($inject);
 ?>
